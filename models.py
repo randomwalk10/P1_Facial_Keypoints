869a82cbc09dc3eaea5b5211431c79ac8036294d
Xuemeng Zhang <nxzhangxm@gmail.com>
2019-04-18 08:28:54 -0700

    still working on 2. Define the Network Architecture.ipynb; keypoints prediction on test images has similar results - not good

--- models.py
+++ models.py
@@ -20,18 +20,44 @@ class Net(nn.Module):
         
         # As an example, you've been given a convolutional layer, which you may (but don't have to) change:
         # 1 input image channel (grayscale), 32 output channels/feature maps, 5x5 square convolution kernel
-        self.conv1 = nn.Conv2d(1, 32, 5)
+        self.conv1 = nn.Conv2d(1, 32, 3, 1, 1)
+        self.conv2 = nn.Conv2d(32, 64, 3, 1, 1)
+        self.conv3 = nn.Conv2d(64, 128, 3, 1, 1)
+        self.conv4 = nn.Conv2d(128, 256, 3, 1, 1)
         
         ## Note that among the layers to add, consider including:
         # maxpooling layers, multiple conv layers, fully-connected layers, and other layers (such as dropout or batch normalization) to avoid overfitting
-        
+        self.pool1 = nn.MaxPool2d(2, 2)
+        self.pool2 = nn.MaxPool2d(2, 2)
+        self.pool3 = nn.MaxPool2d(2, 2)
+        self.pool4 = nn.MaxPool2d(2, 2)
 
+        self.conv1_drop = nn.Dropout2d(0.5)
+        self.conv2_drop = nn.Dropout2d(0.5)
+        self.conv3_drop = nn.Dropout2d(0.5)
+        self.conv4_drop = nn.Dropout2d(0.5)
+
+        self.fc1 = nn.Linear(14*14*256, 2560)
+        self.fc2 = nn.Linear(2560, 136)
+
+        self.fc1_drop = nn.Dropout(0.5)
         
     def forward(self, x):
         ## TODO: Define the feedforward behavior of this model
         ## x is the input image and, as an example, here you may choose to include a pool/conv step:
-        ## x = self.pool(F.relu(self.conv1(x)))
+        x = self.pool1(F.relu(self.conv1(x)))
+        x = self.conv1_drop(x)
+        x = self.pool2(F.relu(self.conv2(x)))
+        x = self.conv2_drop(x)
+        x = self.pool3(F.relu(self.conv3(x)))
+        x = self.conv3_drop(x)
+        x = self.pool4(F.relu(self.conv4(x)))
+        x = self.conv4_drop(x)
+
+        x = x.view(x.size(0), -1)
         
-        
+        x = F.relu(self.fc1(x))
+        x = self.fc1_drop(x)
+        x = self.fc2(x)
         # a modified x, having gone through all the layers of your model, should be returned
         return x
